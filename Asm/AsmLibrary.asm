0c1270ae8282c2695a9b0332ebdc92f1c6931f8c
nipsufn <ferdynand.naczynski@gmail.com>
2016-06-06 14:02:27 +0200

    B&W optimization: constants loaded to registers; pointer used as inerator.

--- Asm/AsmLibrary.asm
+++ Asm/AsmLibrary.asm
@@ -1,7 +1,7 @@
 .data
 ;----------------------------------------------------------------------------------------------
 ;asmBlackAndWhite .data section
-counter		dword 0;
+zero		qword 0;
 
 vectRRRR	real4 0.2126, 0.2126, 0.2126, 0.2126
 vectGGGG	real4 0.7152, 0.7152, 0.7152, 0.7152
@@ -18,29 +18,41 @@ maskGREY	byte 00h, 00h, 00h, 04h, 04h, 04h, 08h, 08h, 08h, 0Ch, 0Ch, 0Ch, 80h, 8
 .code
 
 asmBlackAndWhite proc
-	mov			eax,  dword ptr[r8+8]		;store image length in bytes in EAX
-pixBatch4:
+	mov			rax, zero					;zero RAX
+	mov			eax, dword ptr[r8+8]		;store image length in bytes in EAX
+	add			rax, rcx					;RAX now holds max address in array
+
+	vmovdqu		xmm3, xmmword ptr[maskRRRR]	;store constants in ragisters for faster access
+	vmovdqu		xmm4, xmmword ptr[maskGGGG]
+	vmovdqu		xmm5, xmmword ptr[maskBBBB]
+
+	vmovdqu		xmm6, xmmword ptr[vectRRRR]
+	vmovdqu		xmm7, xmmword ptr[vectGGGG]
+	vmovdqu		xmm8, xmmword ptr[vectBBBB]
+
+	vmovdqu		xmm9, xmmword ptr[maskGREY]
+
+pixBatch4:									;procedure loop
 	vmovdqu		xmm0, xmmword ptr[rcx]		;load data to xmm (too much)
-	vpshufb		xmm2, xmm0, maskRRRR		;shuffle R values to xmm2, just 4 pixels
+	vpshufb		xmm2, xmm0, xmm3			;shuffle R values to xmm2, just 4 pixels
 	vcvtdq2ps	xmm2, xmm2					;convert int -> float
-	vmulps		xmm2, xmm2, vectRRRR		;multiply 4 pixels R
-	vpshufb		xmm1, xmm0, maskGGGG		;shuffle G values to xmm1, just 4 pixels
+	vmulps		xmm2, xmm2, xmm6			;multiply 4 pixels R
+	vpshufb		xmm1, xmm0, xmm4			;shuffle G values to xmm1, just 4 pixels
 	vcvtdq2ps	xmm1, xmm1					;convert int -> float
-	vmulps		xmm1, xmm1, vectGGGG		;multiply 4 pixels G
-	vpshufb		xmm0, xmm0, maskBBBB		;shuffle B values to xmm0, just 4 pixels
+	vmulps		xmm1, xmm1, xmm7			;multiply 4 pixels G
+	vpshufb		xmm0, xmm0, xmm5			;shuffle B values to xmm0, just 4 pixels
 	vcvtdq2ps	xmm0, xmm0					;convert int -> float
-	vmulps		xmm0, xmm0, vectBBBB		;multiply 4 pixels B
+	vmulps		xmm0, xmm0, xmm8			;multiply 4 pixels B
 	vaddps		xmm0, xmm0, xmm1			;add G component to B
 	vaddps		xmm0, xmm0, xmm2			;add R component to G+B
 	vcvtps2dq	xmm0, xmm0					;convert float -> int
-	vpshufb		xmm0, xmm0, maskGREY		;shuffle grey values
+	vpshufb		xmm0, xmm0, xmm9			;shuffle grey values
 	vmovdqu		xmmword ptr[rdx], xmm0		;store 4 processed pixels
 
 	add			rcx, 12						;advance pointer to input 12 bytes (4 pixels)
 	add			rdx, 12						;advance pointer to output 12 bytes (4 pixels)
-	add			counter, 12					;increment counter
-	cmp			counter, eax				;check if bytes counted == bytes in image
-	jng			pixBatch4					;counter not greater than size -> repeat
+	cmp			rcx, rax					;check if max address == current address
+	jng			pixBatch4					;pointer still in range -> repeat
 	ret
 asmBlackAndWhite endp
 
